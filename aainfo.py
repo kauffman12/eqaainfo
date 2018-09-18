3a90362d8941fb2071761a47df225c035a63b70d
John Kauffman <jksoftwarehelp@gmail.com>
2018-09-18 18:02:24 -0400

    included display of spell names and additional AA info also attempt to find new opcode if one changes

--- aainfo.py
+++ aainfo.py
@@ -1,9 +1,10 @@
 # Script for reading network stream from PCAP recording and attempting to parse Everquest AA data
-# Currently working with Test Server version on 9/11/2018
 #
 # EQ Packet handling based on EQExtractor created by EQEMu Development Team
 # --> Copyright (C) 2001-2010 EQEMu Development Team (http://eqemulator.net). Distributed under GPL version 2.
 # 
+# Updated parsing of the AA format to work with the Test Server as of 9/11/2018
+#
 
 import re
 import zlib
@@ -12,10 +13,23 @@ from scapy.all import *
 AATableOpcode = 0x41a4
 OutputFile = 'aainfo.txt'
 DBStringsFile = 'data/dbstr_us.txt'
+DBSpellsFile = 'data/spells_us.txt'
 Debug = False
+
+AATypes = ['Unknown', 'General', 'Archetype', 'Class', 'Special', 'Focus']
+# list of classes in bitmask order
+ClassList = [ 'BerNotUsedHere', 'War', 'Clr', 'Pal', 'Rng', 'SK', 'Dru', 'Mnk', 'Brd', 'Rog', 'Shm', 'Nec', 'Wiz', 'Mag', 'Enc', 'Bst' ]
+
+# Slot 1/SPA info used to search for the AATableOpcode if it is unknown
+# Everyone has these and rank 1 seems to show up after a /resetAA
+WellKnownAAList = [
+  [1, 0, 0, 0, 221, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],  # Packrat
+  [1, 0, 0, 0, 246, 0, 0, 0, 110, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0] # Innate Lung Capacity
+]
 
 Cache = dict()
 DBStrings = dict()
+DBSpells = dict()
 ClientToServer = 0
 ServerToClient = 1
 UnknownDirection = 2
@@ -86,13 +100,38 @@ def uncompress(bytes, opcode, isSubPacket):
     uncompressed = bytes[2:]
   return uncompressed
 
-# Pulls all Titles from EQ DB file. Example/Format: 16366^1^Sorcerer's Vengeance^0^
+# Pulls all Titles from EQ DB files. 
+# DB Srtings Example: 16366^1^Sorcerer's Vengeance^0^
 def loadDBStrings():
-  db = open(DBStringsFile, 'r')
-  for line in db:
-    result = re.match(r'^(\d+)\^[1]\^([\w\s\'\-\(\)\:]+?)\^[0]\^$', line)
-    if (result != None):
-      DBStrings[int(result.group(1))] = result.group(2)
+  try:
+    print('Loading Strings DB from %s' % DBStringsFile)
+    db = open(DBStringsFile, 'r')
+    for line in db:
+      result = re.match(r'^(\d+)\^[1]\^([\w\s\'\-\(\)\:\+]+?)\^[0]\^$', line)
+      if (result != None):
+        DBStrings[int(result.group(1))] = result.group(2)
+    if (len(DBStrings) > 0):
+      print('Found %d entries' % len(DBStrings))
+    else:
+      print('No data found, copy over latest from your EQ directory?')
+  except Exception as error:
+    print(error)
+
+# Spells US Example: 2754^Frenzied Burnout I^
+def loadDBSpells():
+  try:
+    print('Loading Spells DB from %s' % DBSpellsFile)
+    db = open(DBSpellsFile, 'r')
+    for line in db:
+      result = re.match(r'^(\d+)\^([\w\s\'\-\(\)\:\+]+?)\^', line)
+      if (result != None):
+        DBSpells[int(result.group(1))] = result.group(2)
+    if (len(DBSpells) > 0):
+      print('Found %d entries' % len(DBSpells))
+    else:
+      print('No data found, copy over latest from your EQ directory?')
+  except Exception as error:
+    print(error)
 
 def addToCache(seq, direction, bytes, isSubPacket):
   key = 'seq=%d&dir=%d' % (seq, direction)
@@ -117,80 +156,156 @@ def processCache(output, direction):
     del Cache[key]
     processCache(output, direction)
 
+def findOpcode(opcode, buffer):
+  global AATableOpcode
+  result = -1
+
+  # eliminate packets too small for an AA
+  size = len(buffer)
+  if (size >= 140):
+    found = False
+    for aa in WellKnownAAList:
+      start = 0
+      end = len(aa)
+      while (not found and end < size):
+        if (buffer[start:end] == aa):
+          result = 2
+          AATableOpcode = opcode
+          found = True
+        else:
+          start += 1
+          end += 1
+
+  return result
+
 def handleAppPacket(output, srcIP, dstIP, srcPort, dstPort, opcode, size, bytes, pos, direction):
-  result = False
+  result = -1
+  if (AATableOpcode == 0):
+    result = findOpcode(opcode, list(bytes[pos:]))
 
-  if (opcode == AATableOpcode and getDirection(srcIP, dstIP, srcPort, dstPort) == ServerToClient):
-    buffer = list(bytes[pos:])
-    descID = readInt32(buffer)
-    readByte(buffer)
-    hotKeySID = readInt32(buffer)
-    hotKeySID2 = readInt32(buffer)
-    titleSID = readUInt32(buffer)
-    descSID2 = readInt32(buffer)
-    reqLevel = readUInt32(buffer)
-    cost = readUInt32(buffer)
-    aaID = readUInt32(buffer)
-    rank = readUInt32(buffer)
+  if (AATableOpcode != 0 and opcode == AATableOpcode and getDirection(srcIP, dstIP, srcPort, dstPort) == ServerToClient):
+    try:
+      buffer = list(bytes[pos:])
+      descID = readInt32(buffer)
+      readByte(buffer)
+      hotKeySID = readInt32(buffer)
+      hotKeySID2 = readInt32(buffer)
+      titleSID = readInt32(buffer)
+      descSID2 = readInt32(buffer)
+      reqLevel = readUInt32(buffer)
+      cost = readUInt32(buffer)
+      aaID = readUInt32(buffer)
+      rank = readUInt32(buffer)
 
-    reqSkills = readUInt32(buffer)
-    for s in range(reqSkills):
-      readUInt32(buffer)
-    reqPts = readUInt32(buffer)
-    for p in range(reqPts):
-      readUInt32(buffer)
+      reqSkills = []
+      reqSkillCount = readUInt32(buffer)
+      if (reqSkillCount < 5): # or some reasonable value so theres no crazy long loops
+        for s in range(reqSkillCount):
+          value = readUInt32(buffer)
+          if (value > 0):
+            reqSkills.insert(0, value)
+      else:
+        raise('Bad AA Format')
 
-    type = readUInt32(buffer)
-    spellID = readInt32(buffer)
-    readUInt32(buffer) # unknown
-    abilityTimer = readUInt32(buffer)
-    refreshTime = readUInt32(buffer)
-    classMask = readUInt16(buffer)
-    berserkerMask = readUInt16(buffer)
-    maxRank = readUInt32(buffer)
-    prevDescSID = readInt32(buffer)
-    nextDescSID = readInt32(buffer)
-    totalCost = readUInt32(buffer)
-    readBytes(buffer, 10) # unknown
-    expansion = readUInt32(buffer)
-    specialCat = readInt32(buffer)
-    readBytes(buffer, 13) # unknown
-    spaCount = readUInt32(buffer)
+      reqRanks = []
+      reqRankCount = readUInt32(buffer)
+      if (reqRankCount < 5): # or some reasonable value so theres no crazy long loops
+        for p in range(reqRankCount):
+          value = readUInt32(buffer)
+          if (value > 0):
+            reqRanks.insert(0, value)
+      else:
+        raise('Bad AA Format')
 
-    # lookup Title from DB
-    title = DBStrings.get(titleSID)
-    if (title == None):
-      title = titleSID
+      type = readUInt32(buffer)
+      spellID = readInt32(buffer)
+      readUInt32(buffer) # unknown
+      abilityTimer = readUInt32(buffer)
+      refreshTime = readUInt32(buffer)
+      classMask = readUInt16(buffer)
+      berserkerMask = readUInt16(buffer)
+      maxRank = readUInt32(buffer)
+      prevDescSID = readInt32(buffer)
+      nextDescSID = readInt32(buffer)
+      totalCost = readUInt32(buffer)
+      readBytes(buffer, 10) # unknown
+      expansion = readUInt32(buffer)
+      specialCat = readInt32(buffer)
+      readBytes(buffer, 13) # unknown
+      spaCount = readUInt32(buffer)
 
-    output.write('Ability: \t%s\r\n' % title)
-    output.write('Ability ID: \t%d\r\n' % aaID)
-    output.write('Expansion: \t%d\r\n' % expansion)
-    output.write('Level: \t\t%d\r\n' % reqLevel)
-    output.write('Rank: \t\t%d / %d\r\n' % (rank, maxRank))
-    output.write('Rank Cost: \t%d AAs\r\n' % cost)
-    output.write('Reuse Time: \t%ds\r\n' % refreshTime)
+      # lookup Title from DB
+      title = DBStrings.get(titleSID)
+      if (title == None and len(DBStrings) > 0 and titleSID > -1):
+        print('AA Title not found in DB, possible problem parsing data (format change?)')
+      if (title == None):
+        title = titleSID
+      elif (rank > 1):
+        title = '%s (%d)' % (title, rank)
 
-    if (spellID > 0):
-      output.write('Spell ID: \t%d\r\n' % spellID)
-    output.write('Total Cost: \t%d AAs\r\n' % totalCost) 
-    if (spaCount > 0):
-      output.write('Found %d SPAs:\r\n' % spaCount)
+      output.write('Ability: \t%s\r\n' % title)
+      output.write('Activation ID: \t%d\r\n' % aaID)
+      if (type > -1):
+        output.write('Category: \t%s\r\n' % AATypes[type])
 
-    for t in range(spaCount):
-      spa = readUInt32(buffer)
-      base1 = readInt32(buffer)
-      base2 = readInt32(buffer)
-      slot = readUInt32(buffer)
-      output.write('\t\tSlot:\t%d\tSPA:\t%d\tBase1:\t%d\tBase2:\t%d\r\n' % (slot, spa, base1, base2))
+      # list of classes
+      classes = []
+      for c in range(1, len(ClassList)):
+        if ((classMask >> c & 1)):
+          classes.append(ClassList[c]) 
+      
+      if (berserkerMask > 0):
+        classes.append('Ber')
 
-    output.write('\r\n')
-    result = True
+      if (len(classes) == len(ClassList)):
+        output.write('Classes: \tAll\r\n')
+      else:
+        classes.sort()
+        output.write('Classes: \t%s\r\n' % ' '.join(classes))
+
+      output.write('Expansion: \t%d\r\n' % expansion)
+      output.write('Level: \t\t%d\r\n' % reqLevel)
+      output.write('Rank: \t\t%d / %d\r\n' % (rank, maxRank))
+      output.write('Rank Cost: \t%d AAs\r\n' % cost)
+
+      if (refreshTime > 0):
+        output.write('Reuse Time: \t%ds\r\n' % refreshTime)
+        output.write('Timer ID: \t%d\r\n' % abilityTimer)
+
+      if (spellID > 0):
+        spellName = DBSpells.get(spellID)
+        if (spellName == None and len(DBSpells) > 0):
+          print('Spell Title not found in DB for %d, possible problem parsing data (format change?)' % spellID)
+        if (spellName == None):
+          spellName = spellID
+        output.write('Spell: \t\t%s\r\n' % spellName)
+      output.write('Total Cost: \t%d AAs\r\n' % totalCost)
+
+      for i in range(len(reqRanks)):
+        output.write('Requirements: \tRank %d of AA/Skill: %d\r\n' % (reqRanks[i], reqSkills[i]))
+
+      if (spaCount > 0):
+        output.write('Found %d SPA Slots:\r\n' % spaCount)
+
+      for t in range(spaCount):
+        spa = readUInt32(buffer)
+        base1 = readInt32(buffer)
+        base2 = readInt32(buffer)
+        slot = readUInt32(buffer)
+        output.write('\tSlot:\t%d\tSPA:\t%d\tBase1:\t%d\tBase2:\t%d\r\n' % (slot, spa, base1, base2))
+
+      output.write('\r\n')
+      result = 0
+    except Exception as error:
+      if (Debug):
+        print(error)
+
   return result
 
 def handlePacket(output, srcIP, dstIP, srcPort, dstPort, bytes, isSubPacket, isCached):
   global CryptoFlag, FragmentSeq, Fragments, FragmentedPacketSize, FragmentedBytesCollected
   opcode = bytes[0] * 256 + bytes[1]
-  result = False
+  result = -1
 
   direction = getDirection(srcIP, dstIP, srcPort, dstPort)
   if (direction == UnknownDirection and opcode != 1):
@@ -406,34 +521,50 @@ def handlePacket(output, srcIP, dstIP, srcPort, dstPort, bytes, isSubPacket, isC
   return result
 
 def main(args):
+  global AATableOpcode
+
   if (len(args) < 2):
     print ('Usage: ' + args[0] + ' <pcap file>')
   else:
-    try:
-      print('Loading Strings DB from %s' % DBStringsFile)
-      loadDBStrings()
-
-      size = len(DBStrings)
-      if (size > 0):
-        print('Found %d entries' % len(DBStrings))
-      else:
-        print('No data found, copy over latest from your EQ directory?')
-    except Exception as error:
-      print(error)
-
+    loadDBStrings()
+    loadDBSpells()
     output = open(OutputFile, 'w')
-    print('Reading %s' % args[1])
 
     try:
       count = 0
+      print('Reading %s' % args[1])
       packets = rdpcap(args[1])
       for packet in packets:
         if (UDP in packet and len(packet[UDP].payload) > 2):
           result = handlePacket(output, packet[IP].src, packet[IP].dst, packet[UDP].sport, packet[UDP].dport, packet[UDP].payload.load, False, False)
-          if (result):
+          if (result == 0):
             count += 1
 
-      print('Saved data for %d AAs to %s' % (count, OutputFile))
+      if (count > 0):
+        print('Saved data for %d AAs to %s' % (count, OutputFile))
+      else:
+        print('No AAs found using opcode: %s, searching for updated opcode' % hex(AATableOpcode))
+        AATableOpcode = 0
+        for packet in packets:
+          if (UDP in packet and len(packet[UDP].payload) > 2):
+            result = handlePacket(output, packet[IP].src, packet[IP].dst, packet[UDP].sport, packet[UDP].dport, packet[UDP].payload.load, False, False)
+            if (result == 2):
+              break
+        if (AATableOpcode > 0):
+          print('Found likely opcode: %s, trying to parse AA data again' % hex(AATableOpcode))
+          for packet in packets:
+            if (UDP in packet and len(packet[UDP].payload) > 2):
+              result = handlePacket(output, packet[IP].src, packet[IP].dst, packet[UDP].sport, packet[UDP].dport, packet[UDP].payload.load, False, False)
+              if (result == 0):
+                count += 1
+
+          if (count > 0):
+            print('Saved data for %d AAs to %s' % (count, OutputFile))
+            print('Update the default opcode to speed up this process in the future')
+          else:
+            print('AA Format has most likely changed and can not be parsed')
+        else:
+            print('Could not find opcode, giving up')
     except Exception as error:
       print(error)
     output.close()
