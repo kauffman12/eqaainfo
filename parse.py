48334f337bf0a02e60e018fc72c5198bfa184e25
John Kauffman <jksoftwarehelp@gmail.com>
2019-12-03 00:41:28 -0500

    changed spa format for eqcalc

--- parse.py
+++ parse.py
@@ -14,12 +14,14 @@ from lib.eqreader import *
 AATableOpcode = 0x3788 
 OutputFile = 'aainfo.txt'
 
-OutputFormat = 'EQSPELLPARSER'
-#OutputFormat = 'PRETTY'
+#OutputFormat = 'EQSPELLPARSER'
+OutputFormat = 'PRETTY'
 #OutputFormat = 'EQCALC'
 
 Categories = ['', '', 'Progression', '', '', 'Veteran Reward', 'Tradeskill', 'Expendable', 'Racial Innate', 'Everquest', '', 'Item Effect']
 Types = ['Unknown', 'General', 'Archetype', 'Class', 'Special', 'Focus']
+
+FOCUS_SPAS = [ 170, 212, 273, 294, 375, 124, 127, 286, 296, 297, 302, 303, 374, 399, 413, 461, 462, 470, 483, 484, 507 ]
 
 # Slot count + Slot 1/SPA info used to search for the AATableOpcode if it is unknown
 # Everyone has these and rank 1 seems to show up after a /resetAA
@@ -86,20 +88,29 @@ def eqCalcOutput(record):
     entry['id'] = record.aaID
     entry['rank'] = record.rank
 
+    spellFocus = False
+    doesDamage = False
     slotList = []
+
     if (record.spaCount > 0):
       while len(record.spaData) > 3:
-        slots = []
-        slots.append(record.spaData.pop(0))
-        slots.append(record.spaData.pop(0))
-        slots.append(record.spaData.pop(0))
-        slots.insert(0, record.spaData.pop(0))
+        slots = {}
+        slots["spa"] = record.spaData.pop(0)
+        slots["base1"] = record.spaData.pop(0)
+        slots["base2"] = record.spaData.pop(0)
+        slots["num"] = record.spaData.pop(0)
         slotList.append(slots)
+
+        if (slots["spa"] == 0 or slots["spa"] == 79) and slots["base1"] < 0:
+          doesDamage = True
+        if slots["spa"] in FOCUS_SPAS:
+          spellFocus = True
 
     if len(slotList) > 0:
       entry['slotList'] = slotList
-
-    AAData['%s-%02d' % (title, record.rank)] = entry
+    
+    if doesDamage or spellFocus:
+      AAData['%s-%02d' % (title, record.rank)] = entry
 
 def prettyOutput(record):
   title = DBTitleStrings.get(record.titleSID) 
