29e2d9c47b9b2c5517725ba800c87cca047c1df6
John Kauffman <kauffman12@gmail.com>
2020-03-11 20:13:55 -0400

    added max extension for some focus AA spells

--- createdata.py
+++ createdata.py
@@ -11,7 +11,120 @@ NOT_PROC = [ 'Cacophony', 'Necromantic Curse', 'Shock of Magic', 'Fulmination', 
 
 IS_PROC = [ 'Allied Elemental Strike', 'Arcane Fusion', 'Bite of the Asp', 'Blood Pact Strike', 'Cryomancy', 'Decapitation', 'Envenomed Blades Strike', 'Gelid Claw', 'Infusion of Thunder Shock', 'Pyromancy', 'Restless Strike', 'Second Spire of the Savage Lord Strike', 'Storm Blade Strike', 'Synergy Strike', 'Zan Fi\'s Echoes Strike' ]
 
-ADPS_LIST = [ 11, 15, 118, 124, 127, 169, 170, 177, 182, 184, 185, 186, 189, 198, 200, 212, 220, 260, 266, 273, 279, 280, 286, 294, 296, 297, 302, 303, 330, 340, 358, 364, 374, 375, 383, 389, 399, 413, 418, 427, 461, 462, 469, 470, 482, 483, 484, 496, 507 ]
+ADPS_LIST = [ 2, 4, 5, 6, 11, 15, 118, 119, 124, 127, 169, 170, 171, 177, 182, 184, 185, 186, 189, 198, 200, 212, 216, 220, 227, 266, 273, 279, 280, 286, 294, 302, 303, 330, 339, 358, 364, 375, 383, 399, 413, 418, 427, 461, 462, 469, 471, 482, 496, 501, 507 ]
+
+ADPS_B1_MIN = dict()
+ADPS_B1_MIN[11] = 100
+
+ADPS_B1_MAX = dict()
+ADPS_B1_MAX[182] = 0
+
+ADPS_EXT_DUR = dict()
+# bard
+ADPS_EXT_DUR[4516] = 1    # Improved Deftdance Disc
+ADPS_EXT_DUR[8030] = 2    # Improved Thousand Blades Disc
+# beast
+ADPS_EXT_DUR[4671] = 1    # Improved Protective Spirit Disc
+# berserker
+ADPS_EXT_DUR[8003] = 90   # Extended Havoc
+ADPS_EXT_DUR[36556] = 90
+ADPS_EXT_DUR[36557] = 90
+ADPS_EXT_DUR[36558] = 90
+ADPS_EXT_DUR[5041] = 3    # Improved Berserking Disc
+ADPS_EXT_DUR[10923] = 3
+ADPS_EXT_DUR[10924] = 3
+ADPS_EXT_DUR[10925] = 3
+ADPS_EXT_DUR[14189] = 3
+ADPS_EXT_DUR[14190] = 3
+ADPS_EXT_DUR[14191] = 3
+ADPS_EXT_DUR[30463] = 3
+ADPS_EXT_DUR[30464] = 3
+ADPS_EXT_DUR[30465] = 3
+ADPS_EXT_DUR[36529] = 3
+ADPS_EXT_DUR[36530] = 3
+ADPS_EXT_DUR[36531] = 3
+ADPS_EXT_DUR[27257] = 2   # Improved Cleaning Acrimony Disc
+ADPS_EXT_DUR[27258] = 2
+ADPS_EXT_DUR[27259] = 2
+#ranger
+ADPS_EXT_DUR[4506] = 20   # Improved Trueshot Disc
+ADPS_EXT_DUR[15091] = 20
+ADPS_EXT_DUR[15092] = 20
+ADPS_EXT_DUR[15093] = 20
+ADPS_EXT_DUR[19223] = 20
+ADPS_EXT_DUR[19224] = 20
+ADPS_EXT_DUR[19225] = 20
+ADPS_EXT_DUR[25525] = 20
+ADPS_EXT_DUR[25526] = 20
+ADPS_EXT_DUR[25527] = 20
+ADPS_EXT_DUR[40123] = 20
+ADPS_EXT_DUR[40124] = 20
+ADPS_EXT_DUR[40125] = 20
+# rogue
+ADPS_EXT_DUR[35333] = 5   # Extended Aspbleeder Disc
+ADPS_EXT_DUR[35334] = 5
+ADPS_EXT_DUR[35335] = 5
+ADPS_EXT_DUR[44169] = 5
+ADPS_EXT_DUR[44170] = 5
+ADPS_EXT_DUR[44171] = 5
+ADPS_EXT_DUR[56321] = 5
+ADPS_EXT_DUR[56322] = 5
+ADPS_EXT_DUR[56323] = 5
+ADPS_EXT_DUR[59643] = 5
+ADPS_EXT_DUR[59644] = 5
+ADPS_EXT_DUR[59645] = 5
+ADPS_EXT_DUR[35327] = 15  # Improved Fatal Aim Disc
+ADPS_EXT_DUR[35328] = 15
+ADPS_EXT_DUR[35329] = 15
+ADPS_EXT_DUR[56288] = 15 
+ADPS_EXT_DUR[56289] = 15
+ADPS_EXT_DUR[56290] = 15
+ADPS_EXT_DUR[6197] = 2    # Improved Frenzied Stabbing Disc
+ADPS_EXT_DUR[8001] = 90   # Improved Thief's Eyes
+ADPS_EXT_DUR[40294] = 90
+ADPS_EXT_DUR[40295] = 90
+ADPS_EXT_DUR[40296] = 90
+ADPS_EXT_DUR[4695] = 5    # Improved Twisted Chance Disc
+# monk
+ADPS_EXT_DUR[14820] = 3   # Extended Crystalpalm Discipline 
+ADPS_EXT_DUR[14821] = 3
+ADPS_EXT_DUR[14822] = 3
+ADPS_EXT_DUR[18925] = 3
+ADPS_EXT_DUR[18926] = 3
+ADPS_EXT_DUR[18927] = 3
+ADPS_EXT_DUR[29030] = 3
+ADPS_EXT_DUR[29031] = 3
+ADPS_EXT_DUR[29032] = 3
+ADPS_EXT_DUR[35071] = 3
+ADPS_EXT_DUR[35072] = 3
+ADPS_EXT_DUR[35073] = 3
+ADPS_EXT_DUR[8473] = 3    # Extended Heel of Kanji Disc
+ADPS_EXT_DUR[25941] = 3
+ADPS_EXT_DUR[25942] = 3
+ADPS_EXT_DUR[25943] = 3
+ADPS_EXT_DUR[29048] = 3
+ADPS_EXT_DUR[29049] = 3
+ADPS_EXT_DUR[29050] = 3
+ADPS_EXT_DUR[35086] = 3
+ADPS_EXT_DUR[35087] = 3
+ADPS_EXT_DUR[35088] = 3
+ADPS_EXT_DUR[10938] = 2   # Extended Impenetrable Disc
+ADPS_EXT_DUR[10939] = 2
+ADPS_EXT_DUR[10940] = 2
+ADPS_EXT_DUR[10941] = 2
+ADPS_EXT_DUR[10942] = 2
+ADPS_EXT_DUR[10943] = 2
+ADPS_EXT_DUR[4690] = 2
+ADPS_EXT_DUR[35089] = 2
+ADPS_EXT_DUR[35090] = 2
+ADPS_EXT_DUR[35091] = 2
+ADPS_EXT_DUR[11922] = 2   # Improved Scaledfist Disc
+ADPS_EXT_DUR[11923] = 2
+ADPS_EXT_DUR[11924] = 2
+ADPS_EXT_DUR[25923] = 2
+ADPS_EXT_DUR[25924] = 2
+ADPS_EXT_DUR[25925] = 2
+ADPS_EXT_DUR[4691] = 3    # Improved Speed Focus Disc
 
 ALT_NAMES = dict()
 ALT_NAMES['Arms of Holy Wrath I Recourse'] = 'ArmsOfHolyWrathIRecourse'
@@ -89,8 +202,13 @@ if os.path.isfile(DBSpellsFile):
     maxDuration = int(data[12])
     beneficial = int(data[30])
     spellTarget = int(data[32])
+    combatSkill = int(data[101])
     maxHits = int(data[105])
     durationExtendable = int(data[125])
+
+    idNumber = int(id)
+    if idNumber in ADPS_EXT_DUR:
+      maxDuration = maxDuration + ADPS_EXT_DUR[idNumber]
 
     proc = 0
     classMask = 0
@@ -108,21 +226,31 @@ if os.path.isfile(DBSpellsFile):
 	  
     adps = 0
     damaging = 0
-    for spa in data[-1].split('$'):
-      values = spa.split('|')
+    for slot in data[-1].split('$'):
+      values = slot.split('|')
       if len(values) > 1:
-        if beneficial == 0 and (values[1] == '0' or values[1] == '79'):
+        spa = int(values[1])
+        base1 = int(values[2])
+        if beneficial == 0 and (spa == 0 or spa == 79):
           damaging = 1
-        if int(values[1]) in ADPS_LIST:
-          adps = 1
+        
+        if spa in ADPS_LIST:
+          if spa in ADPS_B1_MIN:
+            if base1 >= ADPS_B1_MIN[spa]:
+              adps = 1
+          elif spa in ADPS_B1_MAX:
+            if base1 < ADPS_B1_MAX[spa]:
+              adps = 1
+          elif base1 >= 0:
+            adps = 1
 
     if id in dbStrings:
-      entry = '%s^%s^%d^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, durationExtendable, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, adps)
+      entry = '%s^%s^%d^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, durationExtendable, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, combatSkill, adps)
       myDB.append(entry)
 
       if name in ALT_NAMES:
         name = ALT_NAMES[name]
-        entry = '%s^%s^%d^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, durationExtendable, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, adps)
+        entry = '%s^%s^%d^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, durationExtendable, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, combatSkill, adps)
         myDB.append(entry)
 
   output = open('output.txt', 'w')
