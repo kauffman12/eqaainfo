638b234ae91229dbeb0eb6b7e8a9100aa9899760
John Kauffman <kauffman12@gmail.com>
2020-03-25 20:24:02 -0400

    changed how procs are figured out

--- createdata.py
+++ createdata.py
@@ -5,15 +5,17 @@ DBSpellsStrFile = 'data/spells_us_str.txt'
 
 RANK_LETTERS = [ 'X', 'V', 'I', 'L', 'C', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
-IGNORE_LIST = [ 'MRC -', 'Reserved', 'RESERVED', 'Brittle Haste', 'SKU', 'Type 3', 'Type3', 'BETA', 'Beta', ' Damage ', 'ABTest', 'Test ', ' Test ', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5', 'N/A', 'NA ', 'TEST', 'PH', 'Placeholder' ]
+IGNORE_LIST = [ 'MRC -', 'Reserved', 'AB-Test-TAB', 'SumUNMm', 'RESERVED', 'Brittle Haste', 'SKU', 'Type 3', 'Type3', 'ShapeChange', 'BETA', 'Beta', 'ABTest', 'test ', 'Test ', ' Test ', ' test ', 'Test1', 'Test2', 'Test3', 'Test4', 'Test5', 'N/A', 'NA ', 'TEST', 'PH', 'Placeholder' ]
 
-NOT_PROC = [ 'Cacophony', 'Necromantic Curse', 'Shock of Magic', 'Fulmination', 'Resolution' ]
-
-IS_PROC = [ 'Allied Elemental Strike', 'Arcane Fusion', 'Bite of the Asp', 'Blood Pact Strike', 'Cryomancy', 'Decapitation', 'Envenomed Blades Strike', 'Gelid Claw', 'Infusion of Thunder Shock', 'Pyromancy', 'Restless Strike', 'Second Spire of the Savage Lord Strike', 'Storm Blade Strike', 'Synergy Strike', 'Zan Fi\'s Echoes Strike' ]
+IS_NOT_PROC = [ 'Cloaked Blade', 'Twincast' ] # also appended to later
+IS_PROC = [ 'Arcane Fusion', 'Banestrike', 'Blessing of Life', 'Blessing of the Faithful', 'Bite of the Asp', 'Call of Fire Strike', 'Cascade of Decay Rot', 'Cascading Theft of Defense', 'Cascading Theft of Life', 'Color Shock Stun', 'Cryomancy', 'Decapitation', 'Distracting Strike', 'Divine Surge of Battle', 'Envenomed Blade', 'Eye Gouge', 'Feral Swipe', 'Fists of Fury', 'Flurry of Daggers', 'Frenzied Volley', 'Gelid Claw', 'Gorilla Smash', 'Gut Punch Strike', 'Healing Light', 'Heavy Arrow', 'Hunter\'s Fury', 'Infused by Rage', 'Nature\'s Reprieve', 'Languid Bite', 'Phalanx of Fury', 'Phantasmic Reflex', 'Recourse of Life', 'Sanctified Blessing', 'Uncontained Frenzy', 'Lethality', 'Massive Strike', 'Mortal Coil', 'Overdrive Punch', 'Presence of Fear', 'Pyromancy', 'Reluctant Lifeshare', 'Resonant Kick', 'Resonant Strike', 'Soul Flay', 'Strike of Ire', 'Strike Fury', 'Thunderfoot', 'Theft of Essence', 'Touch of the Cursed', ]
 
 ADPS_CASTER_VALUE = 1
 ADPS_MELEE_VALUE = 2
 ADPS_ALL_VALUE = ADPS_CASTER_VALUE + ADPS_MELEE_VALUE
+
+BASE1_PROC_LIST = [ 85, 419, 427, 429 ]
+BASE2_PROC_LIST = [ 339, 340, 374, 383, 481 ]
 
 ADPS_CASTER = [ 15, 118, 124, 127, 132, 170, 212, 273, 286, 294, 302, 303, 339, 351, 358, 374, 375, 383, 399, 413, 461, 462, 501, 507 ]
 
@@ -134,29 +136,15 @@ ADPS_EXT_DUR[25924] = 2
 ADPS_EXT_DUR[25925] = 2
 ADPS_EXT_DUR[4691] = 3    # Improved Speed Focus Disc
 
-ALT_NAMES = dict()
-ALT_NAMES['Arms of Holy Wrath I Recourse'] = 'ArmsOfHolyWrathIRecourse'
-ALT_NAMES['Arms of Holy Wrath II Recourse'] = 'ArmsOfHolyWrathIIRecourse'
-ALT_NAMES['Arms of Holy Wrath III Recourse'] = 'ArmsOfHolyWrathIIIRecourse'
-ALT_NAMES['Arms of Holy Wrath IV Recourse'] = 'ArmsOfHolyWrathIVRecourse'
-ALT_NAMES['Arms of Holy Wrath V Recourse'] = 'ArmsOfHolyWrathVRecourse'
-ALT_NAMES['Arms of Holy Wrath VI Recourse'] = 'ArmsOfHolyWrathVIRecourse'
-ALT_NAMES['Hand Of Holy Wrath I Recourse'] = 'HandOfHolyWrathIRecourse'
-ALT_NAMES['Hand Of Holy Wrath II Recourse'] = 'HandOfHolyWrathIIRecourse'
-ALT_NAMES['Hand Of Holy Wrath III Recourse'] = 'HandOfHolyWrathIIIRecourse'
-ALT_NAMES['Hand Of Holy Wrath IV Recourse'] = 'HandOfHolyWrathIVRecourse'
-ALT_NAMES['Hand Of Holy Wrath V Recourse'] = 'HandOfHolyWrathVRecourse'
-ALT_NAMES['Hand Of Holy Wrath VI Recourse'] = 'HandOfHolyWrathVIRecourse'
-
-def inNotProcList(name):
-  for test in NOT_PROC:
+def inProcList(name):
+  for test in IS_PROC:
     if test in name:
       return True 
   return False
 
-def inProcList(name):
-  for test in IS_PROC:
-    if test in name:
+def inNotProcList(name):
+  for test in IS_NOT_PROC:
+    if name == test:
       return True 
   return False
 
@@ -195,18 +183,23 @@ if os.path.isfile(DBSpellsStrFile):
   db = open(DBSpellsStrFile, 'r')
   for line in db:
     data = line.split('^')
-    id = data[0]
-    landOnYou = data[3]
-    landOnOther = data[4]
-    dbStrings[id] = { 'landsOnYou': landOnYou, 'landsOnOther': landOnOther }
+
+    try:
+      id = int(data[0])
+      landOnYou = data[3]
+      landOnOther = data[4]
+      dbStrings[id] = { 'landsOnYou': landOnYou, 'landsOnOther': landOnOther }
+    except ValueError:
+      pass
 
 if os.path.isfile(DBSpellsFile):
   print('Loading Spells DB from %s' % DBSpellsFile)
-  db = open(DBSpellsFile, 'r')
-  myDB = []
-  for line in db:
+  myDB = dict()
+  procDB = dict()
+
+  for line in open(DBSpellsFile, 'r'):
     data = line.split('^')
-    id = data[0]
+    id = int(data[0])
     name = data[1]
     if len(name) <= 3:
       continue
@@ -220,7 +213,11 @@ if os.path.isfile(DBSpellsFile):
       continue
 
     abbrv = abbreviate(name)
+    castTime = int(data[8])
+    lockoutTime = int(data[9])
+    recastTime = int(data[10])
     maxDuration = int(data[12])
+    manaCost = int(data[14])
     beneficial = int(data[30])
     spellTarget = int(data[32])
     combatSkill = int(data[101])
@@ -232,11 +229,9 @@ if os.path.isfile(DBSpellsFile):
       maxDuration = maxDuration * 2
 
     # add focus AAs that extend duration
-    idNumber = int(id)
-    if idNumber in ADPS_EXT_DUR:
-      maxDuration = maxDuration + ADPS_EXT_DUR[idNumber]
+    if id in ADPS_EXT_DUR:
+      maxDuration = maxDuration + ADPS_EXT_DUR[id]
 
-    proc = 0
     classMask = 0
     minLevel = 255
     for i in range(38, 38+16):
@@ -245,11 +240,6 @@ if os.path.isfile(DBSpellsFile):
         classMask += (1 << (i - 38))
         minLevel = min(minLevel, level)
 
-    if classMask == 0 and not inNotProcList(name):
-      proc = 1
-    elif classMask != 0 and inProcList(name):
-      proc = 1
-	  
     adps = 0
     damaging = 0
     for slot in data[-1].split('$'):
@@ -257,9 +247,17 @@ if os.path.isfile(DBSpellsFile):
       if len(values) > 1:
         spa = int(values[1])
         base1 = int(values[2])
+        base2 = int(values[3])
         if beneficial == 0 and (spa == 0 or spa == 79):
           damaging = 1
-        
+
+        if spa in BASE1_PROC_LIST:
+          procDB[base1] = spa
+        elif spa in BASE2_PROC_LIST:
+          if spa != 374 and spa != 340 or (manaCost == 0 and castTime == 0):
+            if spa == 85:
+              IS_NOT_PROC.append(abbrv) # there's a proc loop for things
+            procDB[base2] = spa
         if spa in ADPS_LIST:
           if spa in ADPS_B1_MIN:
             if base1 >= ADPS_B1_MIN[spa]:
@@ -270,19 +268,28 @@ if os.path.isfile(DBSpellsFile):
           elif base1 >= 0:
             adps = getAdpsValue(adps, spa)
 
-    if id in dbStrings:
-      spellData = '%s^%s^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, combatSkill, adps)
-      myDB.append(spellData)
+    # ignore long term beneficial buffs like FIRE DAMAGE
+    # howerver allow their SPAs to be checked for procs so continue at the end
+    if maxDuration == 1950 and castTime == 0 and lockoutTime == 0 and recastTime == 0 and beneficial != 0:
+      continue
 
-      if name in ALT_NAMES:
-        name = ALT_NAMES[name]
-        spellData = '%s^%s^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, proc, combatSkill, adps)
-      myDB.append(spellData)
+    if id in dbStrings:
+      spellData = '%s^%s^%d^%d^%d^%d^%d^%d^%s^%s^%d^%d^%d' % (id, name, minLevel, maxDuration, beneficial, maxHits, spellTarget, classMask, dbStrings[id]['landsOnYou'], dbStrings[id]['landsOnOther'], damaging, combatSkill, adps)
+      myDB[id] = dict()
+      myDB[id]['abbrv'] = abbrv
+      myDB[id]['spellData'] = spellData
 
   output = open('output.txt', 'w')
 
   i = 0
-  for spellData in myDB:
+  for spellId in sorted(myDB.keys()):
+    spellInfo = myDB[spellId]
+
+    proc = 0
+    if (spellId in procDB and not inNotProcList(spellInfo['abbrv'])) or inProcList(spellInfo['abbrv']):
+      proc = 1
+
+    spellData = '%s^%d' % (spellInfo['spellData'], proc)
     output.write('%s\n' % spellData)
     i += 1
 
